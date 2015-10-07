13c57d4a931da69674abef85810888ba296bfa58
System Administrator <root@ZhouJunchaos-MacBook-Air.local>
2015-10-07 14:16:43 -0400

    Complete part 1 & 2

--- app/src/main/java/com/example/haotian/tutorial32/MapsActivity.java
+++ app/src/main/java/com/example/haotian/tutorial32/MapsActivity.java
@@ -3,7 +3,9 @@ package com.example.haotian.tutorial32;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -17,15 +19,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MapsActivity extends FragmentActivity {
     public static final String TAG = "MapsActivity";
     public static final int THUMBNAIL = 1;
 
-
-
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Button picButton; //takes user to camera
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +39,42 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
-
         picButton = (Button) findViewById(R.id.photobutton);
 
         picButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dispatchTakePictureIntent();
             }
         });
     }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM+"/Camera");
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        return image;
+    }
+
+    private  void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File tempdir = null;
+
+        if (takePictureIntent.resolveActivity(getPackageManager())!=null){
+            try {
+                tempdir = createImageFile();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            if ( tempdir != null  ){
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempdir));
+            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT);
+        }
+    }
+
 
 
     @Override
